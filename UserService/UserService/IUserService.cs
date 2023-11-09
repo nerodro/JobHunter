93eca217540be4af3e8c9b1c946ed81983c5540f
никита тараканов <ntarak2002@gmail.com>
2023-11-09 19:24:27 +0300

    Delete

--- UserService/UserService/IUserService.cs
+++ UserService/UserService/IUserService.cs
@@ -9,6 +9,10 @@ namespace UserService.UserService
 {
     public interface IUserService
     {
-        IEnumerable<UserModel>
+        IEnumerable<UserModel> GetAll();
+        UserModel GetUser(int id);
+        void Create(UserModel user);
+        void Update(UserModel user);
+        void Delete(int id);
     }
 }
--- UserService/UserService/UserService.cs
+++ UserService/UserService/UserService.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserDomain.Models;
+using UserRepository.UserLogic;
 
 namespace UserService.UserService
 {
-    internal class UserService
+    public class UserService : IUserService
     {
+        private IUserLogic<UserModel> _user;
+        public UserService(IUserLogic<UserModel> user)
+        {
+            _user = user;
+        }
+        public IEnumerable<UserModel> GetAll()
+        {
+            return _user.GetAll();
+        }
+        public UserModel GetUser(int id)
+        {
+            return _user.Get(id);
+        }
+        public void Create(UserModel user)
+        {
+            _user.Create(user);
+        }
+        public void Update(UserModel user)
+        {
+            _user.Update(user);
+        }
+        public void Delete(int id)
+        {
+            UserModel user = GetUser(id);
+            _user.Delete(user);
+            _user.SaveChanges();
+        }
     }
 }
