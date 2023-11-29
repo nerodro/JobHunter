408a2e4130d2e540ebc5a8e1222343e10fbb423e
никита тараканов <ntarak2002@gmail.com>
2023-11-29 19:09:23 +0300

    Map

---
+++ Services/Vacancies/VacancieDomain/Map/ResponseMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacancieDomain.Model;
+
+namespace VacancieDomain.Map
+{
+    public class ResponseMap
+    {
+        public ResponseMap(EntityTypeBuilder<ResponseModel> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(x => x.Id);
+            entityTypeBuilder.Property(x => x.CvId).IsRequired();
+            entityTypeBuilder.Property(x => x.VacancieId).IsRequired();
+            entityTypeBuilder.Property(x => x.Message).IsRequired();
+        }
+    }
+}
---
+++ Services/Vacancies/VacancieDomain/Map/VacancieMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacancieDomain.Model;
+
+namespace VacancieDomain.Map
+{
+    public class VacancieMap
+    {
+        public VacancieMap(EntityTypeBuilder<VacancieModel> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(x => x.Id);
+            entityTypeBuilder.Property(x => x.WorkName).IsRequired();
+            entityTypeBuilder.Property(x => x.AboutWork).IsRequired();
+            entityTypeBuilder.Property(x => x.CompanyId).IsRequired();
+            entityTypeBuilder.Property(x => x.CityId).IsRequired();
+            entityTypeBuilder.Property(x => x.CountryId).IsRequired();
+        }
+    }
+}
