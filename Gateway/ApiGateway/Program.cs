2c69d289a682819efa3f3c8e9128091974c344ce
никита тараканов <ntarak2002@gmail.com>
2024-01-06 15:54:58 +0300

    AuthToken

--- Gateway/ApiGateway/Program.cs
+++ Gateway/ApiGateway/Program.cs
@@ -1,12 +1,32 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
-    
+builder.Services.AddAuthentication(p =>
+{
+    p.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    p.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                 builder.Configuration.GetSection("Jwt:Token").Value!))
+    };
+}).AddCookie();
+
 var app = builder.Build();
 await app.UseOcelot();
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
--- Services/Category/CategoryAPI/Program.cs
+++ Services/Category/CategoryAPI/Program.cs
@@ -62,7 +62,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
+//app.UseAuthentication();
 app.UseAuthorization();
 
 
